54835b493af8875baf48f375a84f72a55fceaf75
gaojianfeng <1396646367@qq.com>
2024-02-22 01:22:29 -0800

    菜品分页显示代码开发

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,6 +1,8 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 
 /**
  * 新增菜品接口
@@ -12,5 +14,10 @@ public interface DishService {
      */
     public  void saveWithFlavor(DishDTO dishDTO);
 
-
+    /**
+     * 菜品分页查询
+     * @param dishPageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
